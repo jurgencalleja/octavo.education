ebae2fea478d9ec9c3dcd1f275af9b4d70f402fc
Jurgen Calleja <hi@jurgencalleja.com>
2021-01-16 19:30:32 +0100

    map fix attempt1

--- js/main.js
+++ js/main.js
@@ -374,8 +374,8 @@ $(document).ready(function () {
         google.maps.event.addDomListener(window, 'load', init);
         function init() {
             var mapOptions = {
-                zoom: 11,
-                center: new google.maps.LatLng(40.6700, -73.9400), // New York
+                zoom: 20,
+                center: new google.maps.LatLng(35.91180204488181, 14.470066439638382), // New York
                 styles: [{
                     "featureType": "water",
                     "elementType": "geometry",
@@ -497,7 +497,7 @@ $(document).ready(function () {
             var mapElement = document.getElementById('map');
             var map = new google.maps.Map(mapElement, mapOptions);
             var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(40.6700, -73.9400),
+                position: new google.maps.LatLng(35.91180204488181, 14.470066439638382),
                 map: map,
                 title: 'Snazzy!'
             });
